3ba009d2dda0822f632453af1c27c63e7fcc3819
raymond machary <raymond.machary@gmail.com>
2024-04-11 16:02:25 +0300

    first commit by machary

---
+++ test.py
@@ -0,0 +1,50 @@
+"""""
+
+# Function to check if a number is even
+def is_even(number):
+    if number % 2 ==0:
+        return True
+    else:
+        return False
+
+#Function to check if a number is odd
+def is_odd(number):
+    if number % 2 != 0:
+        return True
+    else:
+        return False
+#Function to printi whether a number is even or odd
+def print_even_or_odd(number):
+    if is_even(number):
+        print(number,"is even.")
+    else:
+        print(number, "is odd")
+# Main function
+def main():
+    #Get user input for a number
+    num = int(input ("Enter a number"))
+
+    #Print whether the number is even or odd
+    print_even_or_odd(num)
+
+# call the main function to run the program
+main()
+
+test = "Jamila"
+email = fHallow {test}
+it was wonderful to know you 
+Thanks a lot to know you  
+
+print(email)
+
+# Python Module
+
+from calc import add
+try:
+    c = add(5,a)
+    print(c)
+except Exception as e:
+    print("Error ",e)
+"""""
+
+
